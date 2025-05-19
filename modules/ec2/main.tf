c42a721f023468cd5f6ed32f55788c882bf94731
Neslihan Bükte <neslihanbukte20@gmail.com>
2025-05-19 10:47:13 +0300

    add initial vpc and ec2 modules with basic aws infrastructure components

---
+++ modules/ec2/main.tf
@@ -0,0 +1,40 @@
+variable "vpc_id" {}
+variable "subnet_id" {}
+variable "security_group_id" {}
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "terraform-key"
+  public_key = tls_private_key.example.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+  content         = tls_private_key.example.private_key_pem
+  filename        = "${path.module}/terraform-key.pem"
+  file_permission = "0400"
+}
+
+resource "aws_instance" "web" {
+    ami           = "ami-009082a6cd90ccd0e"
+    instance_type = "t2.micro"
+    subnet_id     = var.subnet_id
+    vpc_security_group_ids = [var.security_group_id]
+    associate_public_ip_address = true
+    key_name = aws_key_pair.deployer.key_name
+
+    tags = {
+        Name = "flask-instance"
+    }
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
+
+terraform {
+  backend "local" {}
+}
---
+++ modules/vpc/main.tf
@@ -0,0 +1,76 @@
+resource "aws_vpc" "main" { 
+    cidr_block = "10.0.0.0/16"
+    enable_dns_support = true
+    enable_dns_hostnames = true
+
+    tags = {
+        Name = "flask-vpc"
+    }
+  }
+
+resource "aws_subnet" "public" {  
+    vpc_id     = aws_vpc.main.id
+    cidr_block = "10.0.1.0/24"
+    map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    } 
+}
+
+resource "aws_security_group" "web" {
+  name        = "web-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_route_table_association" "public" {
+    subnet_id      = aws_subnet.public.id
+    route_table_id = aws_route_table.public.id
+}
+
+output "subnet_id" {
+    value = aws_subnet.public.id
+}
+
+output "vpc_id" {
+    value = aws_vpc.main.id
+}
+
+output "security_group_id" {
+  value = aws_security_group.web.id
+}
+
+terraform {
+  backend "local" {}
+}
