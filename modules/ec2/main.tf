c18337c434cf69b5c5df202f17aef0f090e6a69d
Neslihan Bükte <neslihanbukte20@gmail.com>
2025-05-22 14:12:35 +0300

    add outputs for ec2 instance id and arn to support scheduler module

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -40,6 +40,14 @@ output "public_ip" {
   value = aws_instance.web.public_ip
 }
 
+output "web_instance_id" {
+  value = aws_instance.web.id
+}
+
+output "web_instance_arn" {
+  value = aws_instance.web.arn
+}
+
 terraform {
   backend "local" {}
 }
