e5a8422c1c6161b0394e1719a295d7b0cc449c36
Neslihan Bükte <neslihanbukte20@gmail.com>
2025-05-22 14:38:01 +0300

    add ingress rule to enable inbound traffic

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -54,6 +54,13 @@ resource "aws_security_group" "web" {
   }
 
   ingress {
+    from_port = 3000
+    to_port = 3000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port = 9100
     to_port = 9100
     protocol = "tcp"
